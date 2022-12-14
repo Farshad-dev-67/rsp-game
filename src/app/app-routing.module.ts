e7c3ad772d718661a4e5fc0417ead9b93778029e
ali <ali@a.c>
2022-12-14 16:23:53 +0330

    56

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'rps-game',
+    loadChildren: () => import('./component/rps-game/rps-game.module').then(m => m.RpsGameModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/component/rps-game/rps-game.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rps-game',
+  templateUrl: './rps-game.component.html',
+  styleUrls: ['./rps-game.component.scss']
+})
+export class RpsGameComponent {
+
+}
